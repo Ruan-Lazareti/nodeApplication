f522ae1d25a17d1f32ec07c58ea29836e1fef301
ruan <ruanlazareti@hotmail.com>
2023-06-17 10:36:02 -0300

    Create User and eMail verify is completed

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,20 @@
 const knex = require("../database/index.js")
 const { hash, compare } = require("bcryptjs")
+const AppError = require("../utils/AppError.js")
 
 class UserController {
   async create(request, response) {
     const {name, email, password} = request.body
     const hashedPassword = await hash(password, 8)
+    const checkIfEmailExists = await knex("users").where({email})
+
+    if(checkIfEmailExists.length > 0) {
+      throw new AppError("Email já cadastrado!")
+    }
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório!")
+    }
     
     await knex("users")
     .insert({
@@ -13,7 +23,7 @@ class UserController {
       password: hashedPassword
     })
 
-    return response.json()
+    return response.status(201).json()
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,20 @@ const express = require("express")
 const app = express()
 const port = '3333'
 const routes = require("./routes/index.js")
+const AppError = require("./utils/AppError.js")
 app.listen(port)
 app.use(express.json())
 app.use(routes)
+app.use((error, request, response, next) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
